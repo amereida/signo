8162d4df4f178a5d5ffdfeba1c0e38a9264839bf
Herbert Spencer <herbert.spencer@gmail.com>
2017-03-21 20:19:03 -0300

    ble

--- signo_old.js
+++ signo_old.js
@@ -1,26 +1,36 @@
 // Signo
 
 // declaro variables "globales"
-var sides, radius, sw, looping, col;
+var sides, radius, sw, looping, col, pt;
 
-var signo; // es el objeto "signo"
+var s; // es el objeto "signo"
 
 function setup() {
-
-	sides = 9;
-	radius = 300;
-	sw = 5;
-	looping = false;
-	col = color(0);
-
-
+	
 	// construyo el canvas
 	createCanvas(windowWidth, windowHeight);
+	
+	// defino varibales
+	radius = width * .333;
+	sw = 5;
+	looping = true;
+	col = color(0);
+	sides = 9;
+	pt = [sides];
+	
+	// construyo el arreglo de los 9 puntos
+	for (var i = 0; i < pt.length; i++) {
+		var inc = TWO_PI / sides;
+		// son 9 puntos distribuidos en un círculo (identidad circular)
+		var xpos = cos(i * inc) * radius;
+		var ypos = sin(i * inc) * radius;
+		pt[i] = new Point(xpos + width/2, ypos + height/2);
+	}
 
 	// construyo el signo
-	signo = new Signo(width / 2, height * .38, radius);
+	s = new Sign(width / 2, height * .38, radius);
 	// defino sus trazos únicos
-	signo.defineLines();
+	s.defineLines();
 
 	// definiciones gráficas
 	strokeCap(ROUND);
@@ -29,60 +39,55 @@ function setup() {
 
 function draw() {
 
-	/*
-		if (looping) {
-			fill(255, 35);
-			noStroke();
-			rect(0, 0, width, height);
-			s = new Sign(width / 2, height * .38, radius);
-			s.defineLines();
-			// dibujo el blanco
-			sw = 32;
-			col = color(0);
-			s.render();
-			// dibujo el negro
-			sw = 5;
-			col = color(255);
-			s.render();
-		} else {
-			*/
-	background(255);
-	strokeWeight(5);
-	signo.render();
-	// }
+	if (looping) {
+		fill(255, 35);
+		noStroke();
+		rect(0, 0, width, height);
+		s = new Sign(width / 2, height * .38, radius);
+		s.defineLines();
+		// dibujo el blanco
+		sw = 32;
+		col = color(0);
+		s.render();
+		// dibujo el negro
+		sw = 5;
+		col = color(255);
+		s.render();
+	} else {
+		background(255);
+		sw = 5;
+		col = color(0);
+		s.render();
+	}
 }
 
-
-function Signo(x, y) {
+function Point(x, y){
 	this.x = x;
 	this.y = y;
-	var sides = 9;
-	var pt = [sides];
+
+	this.getX = function(){
+		return this.x;
+	}
+	this.getY = function(){
+		return this.y;
+	}
+}
+
+function Sign(x, y) {
 	var numLines = round(random(3, 6));
 	var l = [numLines];
-	// construyo el arreglo de los 9 puntos
-	for (var i = 0; i < pt.length; i++) {
-		var inc = TWO_PI / sides;
-		// son 9 puntos distribuidos en un círculo (identidad circular)
-		var xpos = cos(i * inc) * radius;
-		var ypos = sin(i * inc) * radius;
-		pt[i] = [2];
-		pt[i][0] = xpos;
-		pt[i][1] = ypos;
-	}
+	this.x = x;
+	this.y = y;
 
 	this.defineLines = function() {
 		var count = 0;
 		var start, end;
 		while (count < numLines) {
-			
-			start = round(random(sides - 1));
-			end = round(random(sides - 1));
-
-			print("start = "+start+"     end = "+end);
+			start = pt[round(random(sides-1))];
+			end = pt[round(random(sides-1))];
 
 			if (start != end) {
-				l[count] = new Line(this.pt[start][0], this.pt[start][1], this.pt[end][0], this.pt[end][1]);
+				l[count] = new Line(pt[start], pt[end]);
 				count++;
 			}
 		}
@@ -99,15 +104,15 @@ function Signo(x, y) {
 	}
 }
 
-function Line(x1, y1, x2, y2) {
+function Line(p1, p2) {
 	var cx1, cy1, cx2, cy2; // bezier control points
 	var iscurve;
 
 	// tomos las coordenadas de los puntos (esto no resulta)
-	this.x1 = x1;
-	this.x2 = x2;
-	this.y1 = y1;
-	this.y2 = y2;
+	this.x1 = p1.getX();
+	this.x2 = p2.getX();
+	this.y1 = p1.getY();
+	this.y2 = p2.getY();
 
 	if (random(1) > .5) { // curve-straight probability
 		this.iscurve = true;
