5a6e0270444fb51f72382ac9f5f7825088f746c3
Herbert Spencer <herbert.spencer@gmail.com>
2017-03-21 21:41:20 -0300

    funciona!

--- signo.js
+++ signo.js
@@ -1,143 +1,118 @@
-// Signo
-
-// declaro variables "globales"
-var sides, radius, sw, looping, col;
-
-var signo; // es el objeto "signo"
+var pt;
+var sides;
+var radius;
+var signo;
+var looping;
+var largo; // sólo para ver por qué me queda con 1 trazo el signo
 
 function setup() {
-
-	sides = 9;
-	radius = 300;
-	sw = 5;
-	looping = false;
-	col = color(0);
-
-
-	// construyo el canvas
 	createCanvas(windowWidth, windowHeight);
+	sides = 9;
+	pt = []; // el arreglo (fijo) de 'sides' puntos
+	radius = width / 3; // el radio del signo
+	looping = false;
 
-	// construyo el signo
-	signo = new Signo(width / 2, height * .38, radius);
-	// defino sus trazos únicos
-	signo.defineLines();
+	for (var i = 0; i < 9; i++) {
+		var inc = TWO_PI / 9;
 
-	// definiciones gráficas
-	strokeCap(ROUND);
-	background(255);
-}
-
-function draw() {
-
-	/*
-		if (looping) {
-			fill(255, 35);
-			noStroke();
-			rect(0, 0, width, height);
-			s = new Sign(width / 2, height * .38, radius);
-			s.defineLines();
-			// dibujo el blanco
-			sw = 32;
-			col = color(0);
-			s.render();
-			// dibujo el negro
-			sw = 5;
-			col = color(255);
-			s.render();
-		} else {
-			*/
-	background(255);
-	strokeWeight(5);
-	signo.render();
-	// }
-}
-
-
-function Signo(x, y) {
-	this.x = x;
-	this.y = y;
-	var sides = 9;
-	var pt = [sides];
-	var numLines = round(random(3, 6));
-	var l = [numLines];
-	// construyo el arreglo de los 9 puntos
-	for (var i = 0; i < pt.length; i++) {
-		var inc = TWO_PI / sides;
 		// son 9 puntos distribuidos en un círculo (identidad circular)
 		var xpos = cos(i * inc) * radius;
 		var ypos = sin(i * inc) * radius;
-		pt[i] = [2];
+		pt[i] = [];
 		pt[i][0] = xpos;
 		pt[i][1] = ypos;
 	}
+	signo = new Signo(width / 2, height / 2.5);
+}
 
-	this.defineLines = function() {
-		var count = 0;
-		var start, end;
-		while (count < numLines) {
-			
-			start = round(random(sides - 1));
-			end = round(random(sides - 1));
+function Signo(x, y) {
+	largo = round(random(6, 12));
+	var lin = [largo];
+	var count = 0;
+	var start, end;
 
-			print("start = "+start+"     end = "+end);
+	while (count < lin.length) {
 
-			if (start != end) {
-				l[count] = new Line(this.pt[start][0], this.pt[start][1], this.pt[end][0], this.pt[end][1]);
-				count++;
-			}
+		start = round(random(sides - 1));
+		end = round(random(sides - 1));
+
+		// print("start = " + start + "     end = " + end); // debuging
+
+		if (start != end) {
+			lin[count] = new Linea(start, end);
+			count++;
 		}
 	}
 
 	this.render = function() {
 		push();
 		translate(x, y);
-
-		for (var i = 0; i < l.length; i++) {
-			l[i].render();
+		for (var i = 0; i < lin.length; i++) {
+			lin[i].render();
 		}
 		pop();
 	}
 }
 
-function Line(x1, y1, x2, y2) {
-	var cx1, cy1, cx2, cy2; // bezier control points
-	var iscurve;
+function Linea(a, b) {
+	var x1, x2, y1, y2, cx1, cy1, cx2, cy2;
+	var curve;
 
-	// tomos las coordenadas de los puntos (esto no resulta)
-	this.x1 = x1;
-	this.x2 = x2;
-	this.y1 = y1;
-	this.y2 = y2;
+	x1 = pt[a][0];
+	y1 = pt[a][1];
+	x2 = pt[b][0];
+	y2 = pt[b][1];
 
 	if (random(1) > .5) { // curve-straight probability
-		this.iscurve = true;
+		curve = true;
 	} else {
-		this.iscurve = false;
+		curve = false;
 	}
-	if (iscurve) {
+
+	if (curve) {
 		var d = dist(x1, y1, x2, y2);
-		var a = map(d, 0, radius * 2, 1, 0);
-		this.cx1 = x1 * a + random(-d / 4, d / 4);
-		this.cy1 = y1 * a + random(-d / 4, d / 4);
-		this.cx2 = x2 * a + random(-d / 4, d / 4);
-		this.cy2 = y2 * a + random(-d / 4, d / 4);
+		var amp = map(d, 0, radius * 2, 1, 0);
+		cx1 = x1 * amp + random(-d / 4, d / 4);
+		cy1 = y1 * amp + random(-d / 4, d / 4);
+		cx2 = x2 * amp + random(-d / 4, d / 4);
+		cy2 = y2 * amp + random(-d / 4, d / 4);
 	}
 
 	this.render = function() {
-		noFill();
-		stroke(col);
-		strokeWeight(sw);
-		if (iscurve) {
+		if (curve) {
 			bezier(x1, y1, cx1, cy1, cx2, cy2, x2, y2);
-			strokeWeight(sw);
 		} else {
 			line(x1, y1, x2, y2);
 		}
 	}
 }
 
-function keyPressed() {
+function draw() {
+	noFill();
+	if (looping) {
+		signo = new Signo(width / 2, height / 2.5);
+		strokeWeight(7);
+		stroke(0);
+		signo.render();
+		strokeWeight(5);
+		stroke(255);
+		signo.render();
+		fill(255, 10);
+		noStroke();
+		rect(0, 0, width, height);
+	} else {
+		background(255);
+		strokeWeight(5);
+		stroke(0);
+		signo.render();
+	}
+}
+
+function mouseReleased() {
 	looping = !looping;
-	var filename = "img/" + year() + month() + day() + hour() + minute() + second() + ".png";
-	//saveFrame(filename);
+
+	if (!looping) {
+		fill(0);
+		text(10, height - 30, "el signo tiene " + largo + " trazos");
+	}
 }
